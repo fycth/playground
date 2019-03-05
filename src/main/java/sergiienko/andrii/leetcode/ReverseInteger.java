573fb8f800d3d3fa79acba0a199c0a63b7c2fe8c
Andrii Sergiienko <andrey.sergienko@gmail.com>
2019-03-05 07:47:23 +0200

    adding ReverseInteger task solution

---
+++ src/main/java/sergiienko/andrii/leetcode/ReverseInteger.java
@@ -0,0 +1,16 @@
+package sergiienko.andrii.leetcode;
+
+public class ReverseInteger {
+  public int reverse(int x) {
+    long res = 0;
+    int l = x;
+    while (l != 0) {
+      res = (res * 10) + (l % 10);
+      if (res > Integer.MAX_VALUE || res < Integer.MIN_VALUE) {
+        return 0;
+      }
+      l = l / 10;
+    }
+    return (int)res;
+  }
+}
