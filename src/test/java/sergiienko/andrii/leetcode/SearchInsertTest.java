416dda399e7172fb6c22854698b87fbe7418ddf3
Andrii Sergiienko <andrey.sergienko@gmail.com>
2019-03-05 07:48:10 +0200

    adding SearchInsert task solution

---
+++ src/test/java/sergiienko/andrii/leetcode/SearchInsertTest.java
@@ -0,0 +1,28 @@
+package sergiienko.andrii.leetcode;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SearchInsertTest {
+  @Test
+  public void case1() {
+    assertEquals(2, new SearchInsert().searchInsert(new int[]{1,3,5,6}, 5));
+  }
+  @Test
+  public void case2() {
+    assertEquals(1, new SearchInsert().searchInsert(new int[]{1,3,5,6}, 2));
+  }
+  @Test
+  public void case3() {
+    assertEquals(4, new SearchInsert().searchInsert(new int[]{1,3,5,6}, 7));
+  }
+  @Test
+  public void case4() {
+    assertEquals(0, new SearchInsert().searchInsert(new int[]{1,3,5,6}, 0));
+  }
+  @Test
+  public void case5() {
+    assertEquals(0, new SearchInsert().searchInsert(new int[]{}, 1));
+  }
+}
