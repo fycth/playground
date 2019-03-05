a0cc1d6b6d56a82342a376c9f31c4781482fd8d5
Andrii Sergiienko <andrey.sergienko@gmail.com>
2019-03-05 07:48:29 +0200

    adding StrStr task solution

---
+++ src/test/java/sergiienko/andrii/leetcode/StrStrTest.java
@@ -0,0 +1,18 @@
+package sergiienko.andrii.leetcode;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class StrStrTest {
+  @Test
+  public void genericTest() {
+    assertEquals(2, new StrStr().strStr("hello", "ll"));
+    assertEquals(-1, new StrStr().strStr("aaaaa", "bba"));
+    assertEquals(-1, new StrStr().strStr("", "bba"));
+    assertEquals(0, new StrStr().strStr("", ""));
+    assertEquals(-1, new StrStr().strStr("1", "bba"));
+    assertEquals(0, new StrStr().strStr("12", ""));
+    assertEquals(-1, new StrStr().strStr("missisippi", "issipi"));
+  }
+}
