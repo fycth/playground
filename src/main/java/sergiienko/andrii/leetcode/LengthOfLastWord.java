c343a45e57c5a442925c88b93f101a5ac5d76123
Andrii Sergiienko <andrey.sergienko@gmail.com>
2019-03-05 07:45:05 +0200

    adding LengthOfLastWord task solution

---
+++ src/main/java/sergiienko/andrii/leetcode/LengthOfLastWord.java
@@ -0,0 +1,21 @@
+package sergiienko.andrii.leetcode;
+
+public class LengthOfLastWord {
+  public int lengthOfLastWord(String s) {
+    if (s.length() == 0) {
+      return 0;
+    }
+    int r = s.length();
+    int i = s.length() - 1;
+    if (s.charAt(i) == ' ') {
+      while (i >= 0 && s.charAt(i) == ' ') {
+        i--;
+      }
+      r = i + 1;
+    }
+    while (i >= 0 && s.charAt(i) != ' ') {
+      i--;
+    }
+    return r - i - 1;
+  }
+}
---
+++ src/test/java/sergiienko/andrii/leetcode/LengthOfLastWordTest.java
@@ -0,0 +1,24 @@
+package sergiienko.andrii.leetcode;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class LengthOfLastWordTest {
+  @Test
+  public void case1() {
+    assertEquals(5, new LengthOfLastWord().lengthOfLastWord("Hello World"));
+  }
+  @Test
+  public void case2() {
+    assertEquals(10, new LengthOfLastWord().lengthOfLastWord("HelloWorld"));
+  }
+  @Test
+  public void case3() {
+    assertEquals(0, new LengthOfLastWord().lengthOfLastWord(""));
+  }
+  @Test
+  public void case4() {
+    assertEquals(1, new LengthOfLastWord().lengthOfLastWord("a "));
+  }
+}
