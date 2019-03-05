1ee399497b8ea6cea6a1600146d878841d966800
Andrii Sergiienko <andrey.sergienko@gmail.com>
2019-03-05 07:47:01 +0200

    adding RemoveElement task solution

---
+++ src/test/java/sergiienko/andrii/leetcode/RemoveElementTest.java
@@ -0,0 +1,13 @@
+package sergiienko.andrii.leetcode;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class RemoveElementTest {
+  @Test
+  public void genericTest() {
+    assertEquals(2, new RemoveElement().removeElement(new int[]{3,2,2,3}, 3));
+    assertEquals(5, new RemoveElement().removeElement(new int[]{0,1,2,2,3,0,4,2}, 2));
+  }
+}
