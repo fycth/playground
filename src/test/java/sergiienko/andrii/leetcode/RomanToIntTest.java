3c0f9fce9afb176652e7ab21e569702da5766bb5
Andrii Sergiienko <andrey.sergienko@gmail.com>
2019-03-05 07:47:45 +0200

    adding RomanToInt task solution

---
+++ src/test/java/sergiienko/andrii/leetcode/RomanToIntTest.java
@@ -0,0 +1,16 @@
+package sergiienko.andrii.leetcode;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class RomanToIntTest {
+  @Test
+  public void genericTest() {
+    assertEquals(3, new RomanToInt().romanToInt("III"));
+    assertEquals(4, new RomanToInt().romanToInt("IV"));
+    assertEquals(9, new RomanToInt().romanToInt("IX"));
+    assertEquals(58, new RomanToInt().romanToInt("LVIII"));
+    assertEquals(1994, new RomanToInt().romanToInt("MCMXCIV"));
+  }
+}
