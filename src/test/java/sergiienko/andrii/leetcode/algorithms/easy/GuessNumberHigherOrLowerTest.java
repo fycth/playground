3de11f794505c0f64eb92994bcbf42066927e880
Andrii Sergiienko <andrey.sergienko@gmail.com>
2019-03-07 19:13:57 +0200

    added GuessNumberHigherOrLower solution

---
+++ src/test/java/sergiienko/andrii/leetcode/algorithms/easy/GuessNumberHigherOrLowerTest.java
@@ -0,0 +1,12 @@
+package sergiienko.andrii.leetcode.algorithms.easy;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class GuessNumberHigherOrLowerTest {
+  @Test
+  public void case1() {
+    assertEquals(6, new GuessNumberHigherOrLower().guessNumber(10));
+  }
+}
