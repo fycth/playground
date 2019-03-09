2b20156d3cbada5c33d527c862a22d7eb2af0d76
Andrii Sergiienko <andrey.sergienko@gmail.com>
2019-03-09 20:57:56 +0200

    added InsertionSortList solution

--- src/main/java/sergiienko/andrii/leetcode/algorithms/medium/InsertionSortList.java
+++ src/main/java/sergiienko/andrii/leetcode/algorithms/medium/InsertionSortList.java
@@ -32,4 +32,44 @@ public class InsertionSortList {
       val = x;
     }
   }
+
+  ListNode fromArray(int[] array) {
+    ListNode head = null;
+    if (array.length > 0) {
+      for (int i : array) {
+        ListNode listNode = new ListNode(i);
+        if (head == null) {
+          head = listNode;
+        } else {
+          ListNode h = head;
+          while (h.next != null) {
+            h = h.next;
+          }
+          h.next = listNode;
+        }
+      }
+    }
+    return head;
+  }
+
+  int[] toArray(ListNode head) {
+    ListNode h = head;
+    int l = 0;
+    while (h != null) {
+      h = h.next;
+      l++;
+    }
+    if (l == 0) {
+      return new int[0];
+    }
+    int[] res = new int[l];
+    h = head;
+    int i = 0;
+    while (h != null) {
+      res[i] = h.val;
+      h = h.next;
+      i++;
+    }
+    return res;
+  }
 }
---
+++ src/test/java/sergiienko/andrii/leetcode/algorithms/medium/InsertionSortListTest.java
@@ -0,0 +1,21 @@
+package sergiienko.andrii.leetcode.algorithms.medium;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class InsertionSortListTest {
+  @Test
+  public void case1() {
+    InsertionSortList.ListNode listNode = new InsertionSortList().fromArray(new int[]{4,2,1,3});
+    listNode = new InsertionSortList().insertionSortList(listNode);
+    int[] actual = new InsertionSortList().toArray(listNode);
+    int[] expected = new int[]{1,2,3,4};
+    assertEquals(expected.length, actual.length);
+    int i = 0;
+    while (i < expected.length) {
+      assertEquals(expected[i], actual[i]);
+      i++;
+    }
+  }
+}
