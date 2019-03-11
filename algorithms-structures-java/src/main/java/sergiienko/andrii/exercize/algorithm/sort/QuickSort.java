4ab75e4778a915a652e064b0f2133168f1790f1c
Andrii Sergiienko <andrey.sergienko@gmail.com>
2019-03-11 21:28:00 +0200

    added QuickSort implementation

---
+++ algorithms-structures-java/src/main/java/sergiienko/andrii/exercize/algorithm/sort/QuickSort.java
@@ -0,0 +1,28 @@
+package sergiienko.andrii.exercize.algorithm.sort;
+
+public class QuickSort {
+  public <T extends Comparable<T>> void sort(T[] ints) {
+    quickSort(ints, 0, ints.length - 1);
+  }
+  private <T extends Comparable<T>> void quickSort(T[] ints, int begin, int end) {
+    if (begin >= end) return;
+    T pivot = ints[begin];
+    int l = begin;
+    int h = end;
+    while (true) {
+      while (ints[h].compareTo(pivot) >= 0 && h > l) h--;
+      if (h <= l) ints[l] = pivot;
+      else ints[l] = ints[h];
+      l++;
+      while (ints[l].compareTo(pivot) < 0 && l < h) l++;
+      if (l >= h ) {
+        l = h;
+        ints[h] = pivot;
+      }
+      ints[h] = ints[l];
+      quickSort(ints, begin, l - 1);
+      quickSort(ints, l + 1, end);
+      if (l >= h) break;
+    }
+  }
+}
