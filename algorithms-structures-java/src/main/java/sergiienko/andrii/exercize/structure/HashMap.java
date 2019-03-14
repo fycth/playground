ef05bab17aa1d51f60122dd65b2f222c67b9a2b2
Andrii Sergiienko <andrey.sergienko@gmail.com>
2019-03-14 20:14:23 +0200

    add rehashing to the hashtable impl; fixed some bugs

--- algorithms-structures-java/src/main/java/sergiienko/andrii/exercize/structure/HashMap.java
+++ algorithms-structures-java/src/main/java/sergiienko/andrii/exercize/structure/HashMap.java
@@ -2,13 +2,35 @@ package sergiienko.andrii.exercize.structure;
 
 import java.lang.reflect.Array;
 
+//todo add remove method
+
+//todo add toString method
+
+//todo add getKeys method
+
+//todo add getValues method
+
+//todo add getEntries method
+
 @SuppressWarnings("unchecked")
 public class HashMap<K extends Comparable<K>, V> {
+  private final int DEFAULT_CAPACITY = 10;
   private int size = 0;
-  private MapEntry[] array = (MapEntry[])Array.newInstance(MapEntry.class, 10);
+  private MapEntry[] array = (MapEntry[])Array.newInstance(MapEntry.class, DEFAULT_CAPACITY);
 
   // todo implement re-hash when array is growing or shrinking
   public void put(K key, V value) {
+    if (size >= array.length - array.length / 2) {
+      MapEntry[] arrayOld = array;
+      array = (MapEntry[])Array.newInstance(MapEntry.class, (int)(array.length * 1.5));
+      size = 0;
+      for (MapEntry m : arrayOld) {
+        while (null != m) {
+          put(m.key, m.value);
+          m = m.next;
+        }
+      }
+    }
     int hash = calculateHash(key);
     MapEntry newMapEntry = new MapEntry(key, value, null);
     MapEntry mapEntry = array[hash];
@@ -17,30 +39,34 @@ public class HashMap<K extends Comparable<K>, V> {
       array[hash] = mapEntry;
       size++;
     } else {
-      while (mapEntry.next != null) {
+      while (true) {
+        if (mapEntry.key.compareTo(key) == 0) {
+          mapEntry.value = value;
+          break;
+        }
+        if (mapEntry.next == null) {
+          mapEntry.next = newMapEntry;
+          break;
+        }
         if (mapEntry.next.key.compareTo(key) > 0) {
           newMapEntry.next = mapEntry.next;
           mapEntry.next = newMapEntry;
           break;
-        } else {
-          mapEntry = mapEntry.next;
         }
-      }
-      if (mapEntry.next == null) {
-        mapEntry.next = newMapEntry;
+        mapEntry = mapEntry.next;
       }
     }
   }
 
   private int calculateHash(K key) {
-    return key.hashCode() % array.length;
+    return Math.abs(key.hashCode()) % array.length;
   }
 
   public V get(K key) {
     int hash = calculateHash(key);
     MapEntry mapEntry = array[hash];
     while (mapEntry != null) {
-      if (mapEntry.key == key) break;
+      if (mapEntry.key.compareTo(key) == 0) break;
       mapEntry = mapEntry.next;
     }
     return mapEntry == null ? null : mapEntry.value;
--- algorithms-structures-java/src/test/java/sergiienko/andrii/exercize/structure/HashMapTest.java
+++ algorithms-structures-java/src/test/java/sergiienko/andrii/exercize/structure/HashMapTest.java
@@ -2,6 +2,9 @@ package sergiienko.andrii.exercize.structure;
 
 import org.junit.Test;
 
+import java.util.Map;
+import java.util.Random;
+
 import static org.junit.Assert.assertEquals;
 
 public class HashMapTest {
@@ -12,5 +15,31 @@ public class HashMapTest {
     hashMap.put("1", expected);
     String actual = hashMap.get("1");
     assertEquals(expected, actual);
+  }
+  @Test
+  public void case2() {
+    HashMap<String, String> hashMap = new HashMap<>();
+    String expected = "Hello World !";
+    hashMap.put("1", expected);
+    assertEquals(expected, hashMap.get("1"));
+    hashMap.put("1", "2");
+    assertEquals("2", hashMap.get("1"));
+  }
+  @Test
+  public void case3() {
+    HashMap<String, String> hashMap = new HashMap<>();
+    Map<String, String> m = new java.util.HashMap<>();
+    Random r = new Random();
+    for (int i = 10000; i >= 0; i--) {
+      int key = r.nextInt(Integer.MAX_VALUE);
+      int value = r.nextInt(Integer.MAX_VALUE);
+      m.put(String.valueOf(key), String.valueOf(value));
+      hashMap.put(String.valueOf(key), String.valueOf(value));
+    }
+    for (Map.Entry<String, String> entry : m.entrySet()) {
+      String key = entry.getKey();
+      String value = entry.getValue();
+      assertEquals(value, hashMap.get(key));
+    }
   }
 }
