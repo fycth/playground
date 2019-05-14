00153973e58bfdcf9b0265859b402c6121679f38
Andrii Sergiienko <andrey.sergienko@gmail.com>
2019-05-14 22:24:24 +0300

    add CountPrimes solution

---
+++ src/test/java/sergiienko/andrii/leetcode/algorithms/easy/CountPrimesTest.java
@@ -0,0 +1,12 @@
+package sergiienko.andrii.leetcode.algorithms.easy;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CountPrimesTest {
+  @Test
+  public void case1() {
+    assertEquals(4, new CountPrimes().countPrimes(10));
+  }
+}
