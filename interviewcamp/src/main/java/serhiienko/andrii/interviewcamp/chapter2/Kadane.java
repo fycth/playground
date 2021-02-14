7928d069debd49598bea49c69394b134e268df02
Andrii Sergiienko <andrey.sergienko@gmail.com>
2021-02-14 20:17:35 +0100

    add some comments

--- interviewcamp/src/main/java/serhiienko/andrii/interviewcamp/chapter2/Kadane.java
+++ interviewcamp/src/main/java/serhiienko/andrii/interviewcamp/chapter2/Kadane.java
@@ -1,5 +1,24 @@
 package serhiienko.andrii.interviewcamp.chapter2;
 
+/*
+
+Kadano's algorithm: https://en.wikipedia.org/wiki/Maximum_subarray_problem
+Given an array of integers that can be both +ve and -ve, find the contiguous subarraywith the largest sum.
+For example:  [1,2,-1,2,-3,2,-5]  -> first 4 elements have the largest sum. Return 4
+
+Questions to Clarify:
+
+Q. How do you want the output?
+A. Return the value of the maximum sum.
+
+Q. Do empty arrays count as a subarray?
+A. No, the subarray should have at least 1 element.
+
+Q. But what if the input array is empty or null?
+A. Throw an exception
+
+*/
+
 public class Kadane {
   public int kadane(int[] a) {
     if (a == null || a.length == 0) {
